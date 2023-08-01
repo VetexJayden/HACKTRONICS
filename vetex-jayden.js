6c1ecd2e8f567ab836b7805ef5c2b61eaaff0357
VetexJayden <117556894+VetexJayden@users.noreply.github.com>
2023-08-01 14:10:17 +0300

    vetex-jayden.js

--- vetex-jayden.js
+++ vetex-jayden.js
@@ -50,7 +50,7 @@ async function main() {
   const { state, saveCreds } = await useMultiFileAuthState('session');
   console.log(
     color(
-      figlet.textSync("VETEXJAYDEN  BOT", {
+      figlet.textSync("VETEX-JAYDEN  BOT", {
         font: "Standard",
         horizontalLayout: "default",
         vertivalLayout: "default",
